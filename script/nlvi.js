01cc6e59da97069780d031ec7666aa2a8eb10273
zgshen <zgshen@qq.com>
2018-09-14 09:32:53 +0800

    Site updated: 2018-09-14 09:32:48

--- script/nlvi.js
+++ script/nlvi.js
@@ -51,7 +51,9 @@
         onPicBox () {
             $('.post-content').each(function() {
                 $(this).find('img').each(function() {
-					if (this.height===0 || this.width===0) {
+					if (this.height===0 && this.width!=0) {
+						$(this).replaceWith("<a href='" + this.src + "' data-title='" + this.alt + "' data-lightbox='group'><img src='" + this.src + "' alt='" + this.alt + "' width='"+this.width+"px'></a>")
+					} else if (this.height===0 || this.width===0) {
 						$(this).replaceWith("<a href='" + this.src + "' data-title='" + this.alt + "' data-lightbox='group'><img src='" + this.src + "' alt='" + this.alt + "'></a>")	
 					} else {
 						$(this).replaceWith("<a href='" + this.src + "' data-title='" + this.alt + "' data-lightbox='group'><img src='" + this.src + "' alt='" + this.alt + "' height='"+this.height+"px' width='"+this.width+"px'></a>")
